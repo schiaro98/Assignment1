e3bd6cf5976732e8873a6a946150c7688c67c5f4
luca <luca.rossi57@studio.unibo.it>
2021-03-21 21:52:05 +0100

    tested Page and analysisworker functionalities, monitorto be fixed

---
+++ src/main/java/model/AnalystWorker.java
@@ -0,0 +1,45 @@
+package main.java.model;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class AnalystWorker extends Thread{
+
+    //TODO fare dei test per vedere quanto migliora la situazione con arraylist rispetto a linkedlist
+    private List<Page> pages;
+    private HashMap<String, Integer> pageRank;
+    private RankMonitor rankMonitor;
+    private List<String> unwantedWords;
+
+    public AnalystWorker(String name, List<Page> pages, RankMonitor rankMonitor, List<String> unwantedWords){
+        super(name);
+        this.pages = pages;
+        this.pageRank = new HashMap<>();
+        this.unwantedWords = unwantedWords;
+        this.rankMonitor = rankMonitor;
+    }
+
+    @Override
+    public void run() {
+        Iterator<Page> iterator = pages.iterator();
+        while (iterator.hasNext()){
+            Page p = iterator.next();
+            for (String word : p.getRelevantWords(unwantedWords)){
+                update(word);
+            }
+            iterator.remove();
+            rankMonitor.update(pageRank);
+            pageRank.clear();
+        }
+        System.out.println("Thread "+getName()+" completed his job... exiting");
+    }
+
+    private void update(String word){
+        if(pageRank.containsKey(word)){
+            pageRank.put(word, (pageRank.get(word))+1);
+        } else {
+            pageRank.put(word,1);
+        }
+    }
+}
