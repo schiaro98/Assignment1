08a2ca2f28388e870f462a406dd176bc4baa0868
davide schiaroli <davide.schiaroli2@studio.unibo.it>
2021-03-23 18:56:35 +0100

    Modified controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,21 +1,48 @@
 package main.java.controller;
 
-import main.java.model.Model;
-import main.java.model.RankMonitorImpl;
+import main.java.model.*;
+
+import javax.swing.text.Document;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
 
     private Model model;
+    private List<Page> pages;
+    private RankMonitor monitor = new RankMonitorImpl();
 
     public Controller(RankMonitorImpl model){
         this.model = model;
     }
 
-    public void processEvent(String event){
-        System.out.println(event);//TODO rimuovere
+    public void processEvent(String event) throws IOException {
         switch(event){
             case "confirm":
-                System.out.println("Confirm action required");
+                pages = new documentReader("res/pdf/ps.pdf").getPages();
+                int processors = Runtime.getRuntime().availableProcessors();
+                System.out.println("Processors: " + processors+ " Pages: " + pages.size());
+                int qz = getPagesForThread(processors).get(0);
+                int resto = getPagesForThread(processors).get(1);
+                int acc = 0;
+                new AnalystWorker("0", pages.subList(acc,acc+qz+resto), monitor,
+                    Arrays.asList("AAAAA")).start();
+                acc = acc+qz+resto;
+                System.out.println("Acc:" + acc  );
+                for (int i = 1; i < processors; i++) {
+                    new AnalystWorker(String.valueOf(i), pages.subList(acc,acc+qz), monitor,
+                                Arrays.asList("AAAAA")).start();
+                    acc = acc+qz;
+                    System.out.println("Acc:" + acc  );
+                }
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(monitor.viewMostFrequentN(10));
                 //Faccio partire thread di analisi
                 break;
             case "start":
@@ -27,5 +54,13 @@ public class Controller {
             default:
                 throw new IllegalStateException("Error on action!");
         }
+    }
+
+    private List<Integer> getPagesForThread(int numOfThreads){
+        int numOfPages = pages.size();
+        List<Integer> pagesForThread = new ArrayList<>();
+        pagesForThread.add(numOfPages / numOfThreads);
+        pagesForThread.add(numOfPages % numOfThreads);
+        return pagesForThread;
     }
 }
--- src/main/java/controller/documentReader.java
+++ src/main/java/controller/documentReader.java
@@ -1,31 +1,47 @@
 package main.java.controller;
 
+import main.java.model.Page;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-public class documentReader {
-    private final String text;
+public class documentReader extends PDFTextStripper{
+    //private final String text;
     private final PDDocument document;
     private final Boolean documentPermission;
     private final PDFTextStripper stripper;
+    private final List<Page> pages;
 
     public documentReader(String path) throws IOException {
         this.document = PDDocument.load(new File(path));
         this.documentPermission = document.getCurrentAccessPermission().canExtractContent();
         this.stripper = new PDFTextStripper();
+        pages = new LinkedList<>();
+        System.out.println("INIZIO");
+        /*for (int i = 0; i < document.getNumberOfPages(); i++) {
+            this.stripper.setStartPage(i);
+            this.stripper.setEndPage(i+1);
+            pages.add(new Page(stripper.getText(document).trim()));
+        }*/
         this.stripper.setStartPage(0);
         this.stripper.setEndPage(document.getNumberOfPages());
+        pages.add(new Page(stripper.getText(document).trim()));
+        System.out.println("FINITO");
         //1 doc in più thread
         //Un monitor per ogni pagina/stringa estratta
-        this.text = stripper.getText(document).trim(); //TODO
+        //this.text = stripper.getText(document).trim(); //TODO
     }
 
-    public String getText(){
-        return this.text;
+    public void stripPage(int pageNr) throws IOException {
+        this.setStartPage(pageNr+1);
+        this.setEndPage(pageNr+1);
     }
+
+    //public String getText(){return this.text;}
 
     public PDDocument getDocument() {
         return document;
@@ -42,6 +58,8 @@ public class documentReader {
     public void closeDocument() throws IOException {
         document.close();
     }
+
+    public List<Page> getPages(){ return this.pages; }
 
     public PDFTextStripper getStripper() {
         return stripper;
